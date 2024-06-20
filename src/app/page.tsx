4c054a05654eb197e890860852baf9675b345dce
AntonAA <AntonAAMain@yandex.ru>
2024-06-20 11:18:44 +0300

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
+import { Pagination } from "@/components/features/Pagination/Pagination";
 import { CarFilters } from "@/components/widgets/CarFilters/CarFilters";
 import { CarsList } from "@/components/widgets/CarsList/CarsList";
 
 import styles from "./page.module.css";
-import { Pagination } from "@/components/features/Pagination/Pagination";
 
 export default function Home() {
   return (
